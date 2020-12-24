22c97c3585babfb88fc841c2c540eccacbe90779
amchelapurath2023 <74162023+amchelapurath2023@users.noreply.github.com>
2020-12-24 17:08:41 -0500

    Create CovidSparkAdapter.kt

---
+++ CovidSparkAdapter.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import com.robinhood.spark.SparkAdapter
+
+class CovidSparkAdapter(private val dailyData: List<CovidData>) : SparkAdapter() {
+    override fun getY(index: Int): Float {
+        val chosenDayData = dailyData[index]
+        return chosenDayData.positiveIncrease.toFloat()
+    }
+
+    override fun getItem(index: Int) = dailyData[index]
+
+    override fun getCount() = dailyData.size

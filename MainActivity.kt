68dec09eef1241626487eaceeefe44204d94230c
amchelapurath2023 <74162023+amchelapurath2023@users.noreply.github.com>
2020-12-24 17:06:35 -0500

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,100 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
+import com.google.gson.GsonBuilder
+import com.robinhood.ticker.TickerUtils
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
+private const val TAG = "MainActivity"
+private const val BASE_URL = "https://covidtracking.com/api/v1/"
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var perStateDailyData: Map<String, List<CovidData>>
+    private lateinit var nationalDailyData: List<CovidData>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val gson = GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss").create()
+        val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+        val covidService = retrofit.create(CovidGraph::class.java)
+
+        covidService.getNationalData().enqueue(object : Callback<List<CovidData>> {
+            override fun onFailure(call: Call<List<CovidData>>, t: Throwable) {
+                Log.e(TAG, "onFailure $t")
+            }
+
+            override fun onResponse(call: Call<List<CovidData>>, response: Response<List<CovidData>>) {
+                Log.i(TAG, "onResponse $response")
+                val nationalData = response.body()
+                if (nationalData == null) {
+                    Log.w(TAG, "Did not receive a valid response body")
+                    return
+                }
+
+                nationalDailyData = nationalData.reversed()
+                // TODO: Update graph with national data
+                Log.i(TAG, "TODO: Update graph with national data")
+            }
+        })
+
+        covidService.getStatesData().enqueue(object : Callback<List<CovidData>> {
+            override fun onFailure(call: Call<List<CovidData>>, t: Throwable) {
+                Log.e(TAG, "onFailure $t")
+                updateDisplayWithData(nationalDailyData)
+            }
+
+            override fun onResponse(call: Call<List<CovidData>>, response: Response<List<CovidData>>) {
+                Log.i(TAG, "onResponse $response")
+                val statesData = response.body()
+                if (statesData == null) {
+                    Log.w(TAG, "Did not receive a valid response body")
+                    return
+                }
+
+                perStateDailyData = statesData.reversed().groupBy { it.state }
+                // TODO: Should update spinner with state names
+                Log.i(TAG, "TODO: Should update spinner with state names")
+            }
+        })
+    }
+
+
+    private fun updateDisplayWithData(dailyData: List<CovidData>) {
+        // Create a new SparkAdapter with the data
+        val adapter = CovidSparkAdapter(nationalDailyData)
+        sparkView.adapter = adapter
+        // Update radio buttons to select positive cases and max time by default
+        radioButtonPositive.isChecked = true
+        radioButtonMax.isChecked = true
+        // Display metric for most recent date
+        updateInfoForDate(nationalDailyData.last())
+    }
+
+    private fun updateInfoForDate(covidData: CovidData) {
+        tvMetric.text = NumberFormat.getInstance().format(covidData.positiveIncrease)
+        val outputDateFormat = SimpleDateFormat("MMM dd, yyyy", Locale.US)
+        tvDateLabel.text = outputDateFormat.format(covidData.dateChecked)
+
+    }
+}
+
+
+
